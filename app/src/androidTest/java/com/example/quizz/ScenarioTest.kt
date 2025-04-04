a0aa03ed40cc24bd26a902575e8a52ad777e2f92
ukiD0 <valentinaaakkk@yandex.ru>
2025-04-04 23:00:49 +0300

    QG-18 ui test load case

--- app/src/androidTest/java/com/example/quizz/ScenarioTest.kt
+++ app/src/androidTest/java/com/example/quizz/ScenarioTest.kt
@@ -224,4 +224,25 @@ class ScenarioTest {
         gameOverPage.assertInitialState()
 
     }
+
+    @Test
+    fun caseNumber4() {
+        val loadPage = LoadPage()
+
+        loadPage.assertErrorState()
+        activityScenarioRule.scenario.recreate()
+        loadPage.assertErrorState()
+
+        loadPage.clickRetry()
+
+        loadPage.assertProgressState()
+        activityScenarioRule.scenario.recreate()
+        loadPage.assertProgressState()
+
+        loadPage.waitTillGone()
+
+        gamePage.assertAskedQuestionState()
+        activityScenarioRule.scenario.recreate()
+        gamePage.assertAskedQuestionState()
+    }
 }
